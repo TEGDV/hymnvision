88eb458eca18f3d56d2548398e19a9b412e690bb
Jair Aguilar <Jair Aguilar>
2025-03-05 15:29:34 -0600

    Added SongSectionTypes

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export interface Section {
-  section_type: string; // Matches Rust's `#[serde(rename = "type")]`
+  section_type: SongSectionType; // Matches Rust's `#[serde(rename = "type")]`
   index: number;
   lines: string[];
   chord_pattern: string;
@@ -13,4 +13,16 @@ export interface Song {
   chord_patterns_id: string;
   original_tone: string;
   lyrics: Section[];
+}
+
+export enum SongSectionType {
+  VERSE = "Verse",
+  CHORUS = "Chorus",
+  BRIDGE = "Bridge",
+  INTERLUDE = "Interlude",
+  INTRO = "Intro",
+  OUTRO = "Outro",
+  PRECHORUS = "Pre-Chorus",
+  POSTCHORUS = "Post-Chorus",
+  REFRAIN = "Refrain",
 }
