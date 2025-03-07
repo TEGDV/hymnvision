d54460105b5432b705e4af29dcbf4d3976e17e02
Jair Aguilar <Jair Aguilar>
2025-03-07 00:41:44 -0600

    Added search song command

--- src-tauri/src/models/queries.rs
+++ src-tauri/src/models/queries.rs
@@ -4,8 +4,16 @@ use surrealdb::Surreal;
 use tauri::State;
 
 #[tauri::command]
-pub async fn create_song(db: State<'_, Surreal<Db>>, body: Song) -> surrealdb::Result<()> {
+pub async fn create_song(db: State<'_, Surreal<Db>>, mut body: Song) -> surrealdb::Result<()> {
     let db = db.inner();
+    let full_text = body
+        .lyrics
+        .iter()
+        .map(|section| section.lines.join("\n")) // Join lines within a section
+        .collect::<Vec<_>>() // Collect into a Vec<String>
+        .join(""); // Join sections with a double newline
+                   // Assign the generated text to the field
+    body.full_text = full_text;
     let created: Option<Song> = db
         .create("song")
         .content(body)
@@ -16,12 +24,41 @@ pub async fn create_song(db: State<'_, Surreal<Db>>, body: Song) -> surrealdb::R
 }
 
 #[tauri::command]
-pub async fn query_all(db: State<'_, Surreal<Db>>) -> surrealdb::Result<()> {
+pub async fn query_all(db: State<'_, Surreal<Db>>) -> surrealdb::Result<Vec<Song>> {
     let db = db.inner();
-    let created: Vec<Song> = db
+    let entries: Vec<Song> = db
         .select("song")
         .await
         .expect("Error to create Entry on DB");
-    dbg!(created);
-    Ok(())
+    dbg!(&entries);
+    Ok(entries)
+}
+
+#[tauri::command]
+pub async fn search_song(
+    db: State<'_, Surreal<Db>>,
+    query: String,
+) -> surrealdb::Result<Vec<Song>> {
+    let db = db.inner();
+
+    // Split query into words (tokens)
+    let words: Vec<&str> = query.split_whitespace().collect();
+
+    // Generate WHERE clause dynamically
+    let where_clause = words
+        .iter()
+        .map(|word| format!("full_text @@ '{}'", word)) // Format each token
+        .collect::<Vec<String>>()
+        .join(" OR ");
+    let sql = format!(
+        "SELECT * FROM song WHERE {} ORDER BY title DESC;",
+        where_clause
+    );
+
+    let mut response = db.query(sql).await.expect("Error to create Entry on DB");
+    let entries = response
+        .take(0)
+        .expect("Error during desirializing Song Search");
+    dbg!(&entries);
+    Ok(entries)
 }
