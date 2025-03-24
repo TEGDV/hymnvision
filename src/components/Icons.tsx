7ddd1920598fdec5fd6917285b5d5266e7a1b502
Jair Aguilar <Jair Aguilar>
2025-03-24 17:38:05 -0600

    Added Next/Prev Lines and ZoomIn/Out Controls

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -189,3 +189,161 @@ export function IconTrash({ className = "" }) {
     </svg>
   );
 }
+
+export function IconPlay({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+      />
+    </svg>
+  );
+}
+
+
+export function IconStop({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"
+      />
+    </svg>
+  );
+}
+
+export function IconArrowRight({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+      />
+    </svg>
+  );
+}
+
+export function IconArrowLeft({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+      />
+    </svg>
+  );
+}
+
+export function IconEye({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+      />
+    </svg>
+  );
+}
+
+export function IconEyeSlash({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+      />
+    </svg>
+  );
+}
+
+export function IconZoomIn({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607ZM10.5 7.5v6m3-3h-6"
+      />
+    </svg>
+  );
+}
+
+export function IconZoomOut({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607ZM13.5 10.5h-6"
+      />
+    </svg>
+  );
+}
--- src/components/PresentationView.tsx
+++ src/components/PresentationView.tsx
@@ -1,8 +1,9 @@
 import Card from "./Card.tsx"
-import { createSignal, onMount, For, Index, Show} from "solid-js";
+import { createSignal, createMemo,onMount, batch, For, Index, Show} from "solid-js";
 import {Song} from "../types";
 import SearchSongComponent from "./SearchSongComponent"
 import { createStore } from "solid-js/store";
+import {IconEye, IconEyeSlash, IconPlay, IconStop, IconArrowRight, IconArrowLeft, IconZoomIn, IconZoomOut} from "./Icons";
 
 function PresentationView() {
   const [songs, setSongs] = createStore<{entries: Song[]}>({entries: []})
@@ -11,32 +12,74 @@ function PresentationView() {
   const [activeSong, setActiveSong] = createSignal<Song | null>(null);
   const [currentLines,setCurrentLines] = createSignal([""])
   const [currentPage,setCurrentPage] = createSignal(0)
-  const [presentationControls, setPresentationControls] = createSignal({
-    textSize: 1,
-    displayedLines: 2,
-    textStyle: "font-techno",
-    backgroundAnimation: "Default"
+  const currentLyrics = createMemo(() => activeSong()?.lyrics ?? []);
+  const firstSectionLines = createMemo(() => currentLyrics()[0]?.lines ?? []);
+  // Controls Signals
+
+
+  const [textSize, setTextSize] = createSignal(2);
+  const [displayedLines, setDisplayedLines] = createSignal(2);
+  const [textStyle, setTextStyle] = createSignal("font-techno");
+  const [backgroundAnimation, setBackgroundAnimation] = createSignal("default");
+  const [page, setPage] = createSignal(0);
+  const fontSizeClass = createMemo(() => {
+    const size = textSize();
+    return {
+      1: "text-xl",
+      2: "text-2xl",
+      3: "text-3xl",
+      4: "text-4xl",
+      5: "text-5xl",
+      6: "text-6xl",
+      7: "text-7xl",
+    }[size] ?? "text-xl";
   });
   // PresentationCommands
+
   // Styling
+  function zoomIn() {
+    setTextSize((size) => Math.min(size + 1, 7));
+  }
+
+  function zoomOut() {
+    setTextSize((size) => Math.max(size - 1, 1));
+  }
 
   // Controls
   function nextLines() {
-    const song = activeSong(); // Access signal value
-    const page = currentPage(); // Current index
-    // Validate song and lyrics array
+    const song = activeSong();
+    const currentPage = page();
+
     if (!song?.lyrics || song.lyrics.length === 0) return;
 
-    // Get the current page or loop to 0 if invalid
-    const validPage = song.lyrics[page] ? page : 0;
+    const nextPage = (currentPage + 1) % song.lyrics.length;
 
-    // Set current lines from valid page
-    setCurrentLines(song.lyrics[validPage].lines);
+    const lines = song.lyrics[nextPage]?.lines ?? [];
 
-    // Increment page, loop back to 0 if at the end
-    const nextPage = (validPage + 1) % song.lyrics.length;
-    setCurrentPage(nextPage);
+    batch(() => {
+      setPage(nextPage);
+      setCurrentLines(lines);
+    });
   }
+
+  function prevLines() {
+    const song = activeSong();
+    const currentPage = page();
+
+    if (!song?.lyrics || song.lyrics.length === 0) return;
+
+    if (currentPage <= 0) return;
+
+    const prevPage = currentPage - 1;
+
+    const lines = song.lyrics[prevPage]?.lines ?? [];
+
+    batch(() => {
+      setPage(prevPage);
+      setCurrentLines(lines);
+    });
+  }
+
   return (<>
     <main class="grid gap-2 grid-cols-12 grid-rows-[32px_repeat(7,1fr)] h-full w-full p-2">
       <div class="ctr text-sm">Play Button</div>
@@ -59,26 +102,33 @@ function PresentationView() {
       </div>
       <div class="ctr flex-wrap container-primary row-start-7 col-span-4 row-span-2 w-full h-full">
         <div class="bg-neutral-950/50 w-full">
-          <p class="">Content Here</p>
+          <Show when={firstSectionLines().length}>
+              <p class="whitespace-pre">{firstSectionLines().join("\n")}</p>
+          </Show>
         </div>
       </div>
       <div class="container-primary row-start-2 col-start-5 col-span-4 row-span-full bg-green-900 grid auto-cols-min overflow-scroll">
-        <Show when={activeSong() != null}>
-          <For each={activeSong().lyrics}>
+        <Show when={currentLyrics().length}>
+          <For each={currentLyrics()}>
             {(section, index) => 
               <p class="text-md m-2 whitespace-pre"><b>{section.section_type}</b><br/>{section.lines.join("\n")}</p>
             }
           </For>
         </Show>
       </div>
-      <div onClick={() => nextLines()} class="ctr flex-wrap container-primary row-start-2 col-start-9 col-span-4 row-span-3">
+      <div  class="ctr relative flex-wrap container-primary row-start-2 col-start-9 col-span-4 row-span-3">
         <div class="bg-neutral-950/50 w-full">
           <For each={currentLines()}>
             {(line) => 
-              <p class={`${presentationControls().textStyle} text-${presentationControls().textSize}xl `}>{line}</p>
+              <p class={`${textStyle()} ${fontSizeClass()}`}>{line}</p>
             }
           </For>
         </div>
+        <button onClick={prevLines} class="mx-2 absolute bottom-2 right-[120px] z-50 bg-blue-100"><IconArrowLeft className="md-icon"/></button>
+        <button onClick={nextLines} class="mx-2 absolute bottom-2 right-[94px] z-50"><IconArrowRight className="md-icon"/></button>
+        <button class="mx-2 absolute bottom-2 right-[68px] z-50"><IconEye className="md-icon" /></button>
+        <button onClick={zoomIn} class="mx-2 absolute bottom-2 right-[42px] z-50"><IconZoomIn className="md-icon" /></button>
+        <button onClick={zoomOut} class=" mx-2 absolute bottom-2 right-[16px] z-50"><IconZoomOut className="md-icon" /></button>
       </div>
       <div class="ctr container-primary col-start-9 col-span-4 row-span-4 bg-teal-400">Quick References</div>
     </main>
